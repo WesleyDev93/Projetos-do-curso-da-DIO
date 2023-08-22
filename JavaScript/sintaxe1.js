0dd57f5cd9cb8704d94ead0a909d0ee903f15b6e
WesleyDev93 <wesleyoliveiralimadasilva@gmail.com>
2023-08-22 10:25:25 -0300

    Subindo projeto de estudo de JS

---
+++ JavaScript/sintaxe1.js
@@ -0,0 +1,27 @@
+
+
+
+const input =  Number(prompt("Selecione a operação 1 - soma ,  2 - subitração,  3 -multipliação,  4 - potenciação"));
+
+
+console.log (input); 
+
+
+let pegaNumero1 = Number(prompt("Digite o numero 1"));
+let pegaNumero2 = Number(prompt("Digite o numero 2"));
+let resultado;
+
+console.log(pegaNumero1, pegaNumero2);
+   
+
+
+function validaDados (input) {
+    
+    if () {
+        
+    }
+
+
+}
+
+validaDados();
