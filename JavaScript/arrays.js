494521eb9c4ba33d96b8511ffe1a6aefbb2b48d2
WesleyDev93 <wesleyoliveiralimadasilva@gmail.com>
2023-08-22 11:50:47 -0300

    Inserindo Dados no Array

---
+++ JavaScript/arrays.js
@@ -0,0 +1,27 @@
+
+
+
+let numeros = [];
+
+
+
+
+
+
+function incluirDados(input) {
+    
+    if (input) {
+        numeros.push(input); 
+    }
+
+    alert (numeros);
+    
+}
+
+console.log (numeros);
+
+    
+const input = Number(prompt("Digite o numero que sera incluido no array"));
+incluirDados(input);
+
+
