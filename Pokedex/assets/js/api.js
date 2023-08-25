36062b327cbf50d8562ee40d957c3c247a40c0ce
WesleyDev93 <wesleyoliveiralimadasilva@gmail.com>
2023-08-25 16:03:26 -0300

    Criando botao de carregar mais pokemons , e implementando nova feature

--- Pokedex/assets/js/api.js
+++ Pokedex/assets/js/api.js
@@ -3,6 +3,17 @@
  //   return pokemonTypes.map ((typeSlot) => `<li class="tipo">${}</li>`)
 //}  não sera usado pois criamos uma Class , com os dados de acordo com nosso projeto
 
+
+const pokemonOL = document.getElementById ('pokemonList') // pega pelo ID a ol pai
+const botao = document.getElementById ('carregarMais')// pega pelo ID o botao
+const limit = 5
+let offset = 0
+
+
+
+
+
+
 function convertePokemonLI(pokemon) { // cria template da LI 
      return `<li class="item1 ${pokemon.types}">
                 
@@ -17,27 +28,55 @@ function convertePokemonLI(pokemon) { // cria template da LI
          </ol>
 
          <img class="imagem" src="${pokemon.foto}" alt="${pokemon.name}">
+
+         
      </div>
      `
-
-}
 // se atentar na busca das propriedades na API para ver o caminho.
-
- const pokemonOL = document.getElementById ('pokemonList') // pega pelo ID a ol pai
- 
+}
 
 
 
 
-  pokeAPI.getPokemons ().then ((pokemonList = []) => {
 
-     const  novaHTML = pokemonList.map(convertePokemonLI).join ('')  //função transforma uma forma em outra forma 
-     pokemonOL.innerHTML = novaHTML
-    
-         
+// nova função de paginação 
 
 
-})
+function carregarPokemonItens(offset, limit) {
+    pokeAPI.getPokemons (offset, limit).then ((pokemonList = []) => {
+
+        const  novaHTML = pokemonList.map(convertePokemonLI).join ('')  //função transforma uma forma em outra forma 
+        pokemonOL.innerHTML += novaHTML
+       
+            
+   
+   
+   })
+}
+
+
+
+carregarPokemonItens (offset, limit)
+
+
+
+botao.addEventListener ('click', () => {
+    offset += limit
+    carregarPokemonItens (offset, limit)
+}
+)
+
+
+
+
+
+
+
+ // pokeAPI.getPokemons ().then ((pokemonList = []) => {
+
+ //    const  novaHTML = pokemonList.map(convertePokemonLI).join ('')  //função transforma uma forma em outra forma 
+ //    pokemonOL.innerHTML = novaHTML
+//})
      
 
     
