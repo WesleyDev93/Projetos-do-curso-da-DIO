1fd9cfddca4b6053bda38cad4e1637cf94bf38f2
WesleyDev93 <wesleyoliveiralimadasilva@gmail.com>
2023-08-25 11:05:22 -0300

    Separando dados da API

--- Pokedex/assets/js/api.js
+++ Pokedex/assets/js/api.js
@@ -1,7 +1,32 @@
 
-const  offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+
+
+function convertePokemonLI(pokemon) { // cria template da LI 
+     return `<li class="item1">
+                
+     <span class="numero">#001</span>
+     <span class="nome">${pokemon.name}</span>
+     
+     
+     <div class="detalhes">
+         <ol class="tipos">
+
+             <li class="tipo">Tipo 01</li>
+             <li class="tipo">Tipo 02</li>
+         </ol>
+
+         <img class="imagem" src="./assets/img/teste.png" alt="${pokemon.name}">
+     </div>
+     
+ </li>`
+
+}
+
+
+ const pokemonOL = document.getElementById ('pokemonList') // pega pelo ID a ol pai
+ 
+
 
 
 
@@ -9,7 +34,18 @@ fetch (url) // gera um processamento no servidor e uma resposta em promessa
 
 .then ( (response) => { return response.json ()})   // convertendo em Json  => arrow function
 
-.then ( (response) => {  console.log(response)  })  // Body convertido 
+.then ( (jsonBody) => jsonBody.results)  // Body convertido 
+
+.then ((pokemonList) => {
+    
+    for (let i = 0; i < pokemonList.length; i++) {
+    const pokemon = pokemonList[i]
+    pokemonOL.innerHTML += convertePokemonLI(pokemon) // traz a lista para LI 
+
+
+    
+    
+}})
     
 .catch ( (erro) => {  console.log("Concluida") })  // tratamento de erros 
      
