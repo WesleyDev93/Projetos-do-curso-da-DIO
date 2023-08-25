43e8afd6022528d66488e7fe231871ef17e40e16
WesleyDev93 <wesleyoliveiralimadasilva@gmail.com>
2023-08-25 14:23:03 -0300

    Subindo alterações restantes do ultimo Commit

--- Pokedex/assets/js/api.js
+++ Pokedex/assets/js/api.js
@@ -1,31 +1,27 @@
 
-
-const offset = 0
-const limit = 10
- const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-
+//function convertePokemonTypesOL(pokemonTypes) {
+ //   return pokemonTypes.map ((typeSlot) => `<li class="tipo">${}</li>`)
+//}  não sera usado pois criamos uma Class , com os dados de acordo com nosso projeto
 
 function convertePokemonLI(pokemon) { // cria template da LI 
      return `<li class="item1">
                 
-     <span class="numero">#001</span>
+     <span class="numero">${pokemon.numero}</span>
      <span class="nome">${pokemon.name}</span>
      
      
      <div class="detalhes">
          <ol class="tipos">
-
-             <li class="tipo">Tipo 01</li>
-             <li class="tipo">Tipo 02</li>
+              
+            ${convertePokemonTypesOL(pokemon.types).join ('')}
          </ol>
 
-         <img class="imagem" src="./assets/img/teste.png" alt="${pokemon.name}">
+         <img class="imagem" src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
      </div>
-     
- </li>`
+     `
 
 }
-
+// se atentar na busca das propriedades na API para ver o caminho.
 
  const pokemonOL = document.getElementById ('pokemonList') // pega pelo ID a ol pai
  
@@ -33,20 +29,14 @@ function convertePokemonLI(pokemon) { // cria template da LI
 
 
 
-pokeAPI.getPokemons ().then ((pokemonList) => {
+  pokeAPI.getPokemons ().then ((pokemonList = []) => {
 
-    const listaItem = []
+     const  novaHTML = pokemonList.map(convertePokemonLI).join ('')  //função transforma uma forma em outra forma 
+     pokemonOL.innerHTML = novaHTML
+    
+         
 
-    pokemon.map ()     //função transforma uma forma em outra forma 
 
-    for (let i = 0; i < pokemonList.length; i++) {
-    const pokemon = pokemonList[i]
-    listaItem.push (convertePokemonLI(pokemon)) // Converte diretamente a lista , carregando ela de uma vez.
-   
-    console.log (listaItem)
-
-}
-//pokemonOL.innerHTML += convertePokemonLI(pokemon) // traz a lista para LI -- retirado para nao recarregar toda vez
 })
      
 
--- Pokedex/assets/js/home.js
+++ Pokedex/assets/js/home.js
@@ -2,6 +2,35 @@
 
 const pokeAPI =  {}
 
+
+
+function convertPokeaiDetailPokemon (pokemonDetail) {
+    const pokemon = new pokemon()
+    pokemon.numero = pokemonDetail.order
+    pokemon.name = pokemonDetail.name
+    const types = pokemonDetail.types.map ((typeSlot) => typeSlot.type.name)
+    const [type] = types
+    pokemon.types = types
+    pokemon.type = type
+
+
+
+    pokemon.foto = pokemonDetail.sprites.other.dream_world
+    
+
+    return pokemon 
+
+}
+
+pokeAPI.getPokemonDetail = (pokemon) => {
+    return fetch (pokemon.url)
+    .then((response) => response.json())
+    .then (convertPokeaiDetailPokemon)
+}
+
+
+
+
 pokeAPI.getPokemons = ( offset = 0, limit = 10 ) =>  {
 
     
@@ -11,11 +40,26 @@ pokeAPI.getPokemons = ( offset = 0, limit = 10 ) =>  {
     
     return fetch (url) // gera um processamento no servidor e uma resposta em promessa
 
-    .then ( (response) => { return response.json ()})   // convertendo em Json  => arrow function
+    .then ( (response) =>  response.json ())   // convertendo em Json  => arrow function
 
     .then ( (jsonBody) => jsonBody.results)  // Body convertido 
-      
-    .catch ( (erro) => {  console.log("Concluida") })  // tratamento de erros 
 
-    .finally ( () => {  console.log ("O serviço foi concluido")})  // mensagem final independente de erros
+    .then ((pokemons)  => pokemons.map (pokeAPI.getPokemonDetail))
+                             
+
+    .then ((detailRequests) => Promise.all(detailRequests))
+
+    .then ((pokemonDetails) => pokemonDetails)
+      
+    
 }
+
+
+
+
+
+// Prestar atenção na escrita correta das palavras reservadas.
+
+//.catch ( (erro) => {  console.log("Concluida") })  // tratamento de erros 
+
+    //.finally ( () => {  console.log ("O serviço foi concluido")})  // mensagem final independente  de erros
