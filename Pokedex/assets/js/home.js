afdb579612c4e1b5cf76a303bdc26d6a16e8be6e
WesleyDev93 <wesleyoliveiralimadasilva@gmail.com>
2023-08-25 11:05:40 -0300

    subindo commits

---
+++ Pokedex/assets/js/home.js
@@ -0,0 +1,11 @@
+
+
+const pokeAPI =  {}
+
+pokeAPI.getPokemons = ( offset = 0, limit = 10 ) =>  {
+
+    const  offset = 0;
+    const limit = 10;
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+}
