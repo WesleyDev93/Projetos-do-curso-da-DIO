69ece651f1ad0f9c6fc597024fbe55f5f679c8e1
WesleyDev93 <wesleyoliveiralimadasilva@gmail.com>
2023-09-04 11:51:55 -0300

    Aprendendo sobre Classes e Sub Classes

--- TypeScript/src/classes.ts
+++ TypeScript/src/classes.ts
@@ -68,7 +68,71 @@ const p  = new belo (5, "Wesley");
 
 
 
-// Classes 
+// Classes   (forma)
+
+class personagem {
+
+    
+    name:string;
+    // para adicionar um atributo opcional  ?
+    // name?: string;  Assim não precisamos pasar o nome se não quisermos.
+    forca: number;
+    poder: number;
+
+    constructor (name: string, forca: number,poder: number,) {
+           
+        this.name = name;
+        this.forca = forca;
+        this.poder = poder;
+
+    }
+
+    ataque (): void {
+
+        console.log (`Ataque tem  ${this.forca} pontos`)
+    }
+}
+
+
+
+// criar o elemento com os parametros 
+
+
+const p1 = new personagem ("Capitão America", 1250, 350)
+p1.ataque ()
+
+
+// Data Modifiers  (modificadores de acesso) -- Public -- Private -- Protected --
+
+
+    // ao Adicionar o private , o item so pode ser acessado atraves da classe pelo construtor
+    //não havera a opção caso usarmos o p2.name pois o atributo name esta privado.
+    // o metodo public já e default do sistema , pode ser declarado mas pode padrão ele é public.
+    // ao Adicionar o private , o item so pode ser acessado atraves da classes e subclasses
+    // Os modificadores podem ser usados em metodos tambem
+
+
+    // Sub Classes , conceito de herança , ganhar atributos de outra classe por herança
+    // A classe que é pai sempre sera a superClass
+    // A classe filha ou subClasse será nosso mago
+
+    class mago extends personagem {
+
+        // adicionar novos atributos desta subClass
+
+        magicPontos: number;
+
+        constructor (name: string, forca:number, poder: number, magicPontos: number) {
+              
+            
+
+        // usamos o super para invocar o construtor da classe pai -- atenção aos parametros
+            super (name, forca, poder); 
+            this.magicPontos = magicPontos; // Declarando com o this 
+        }
+    }
+// construimos o personagem mago , com todos os atributos anteriores e mais novo que é o magicPontos
+    const p3 = new mago ("mago negro", 1500,450,800)
 
 
 
